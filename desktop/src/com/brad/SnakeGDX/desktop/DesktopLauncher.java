db9fa11296e97d4d55d79625eab29384b6b57711
Bradley Anderson <Brad.Anderson1995@gmail.com>
2014-12-30 17:13:50 -0600

    Initial commit

---
+++ desktop/src/com/brad/SnakeGDX/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brad.SnakeGDX.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brad.SnakeGDX.SnakeGDX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SnakeGDX(), config);
+	}
+}
